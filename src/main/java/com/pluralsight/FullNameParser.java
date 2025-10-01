bcda898f47e62226c572fba79174c68e037f7127
keisha De La Rosa <keishdlr@gmail.com>
2025-10-01 12:50:19 -0400

    added output

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -10,26 +10,26 @@ public class FullNameParser {
     public static void main(String[] args) {
 
         System.out.println(" Choose a format to enter your name (without middle/with middle)"); // prompt the user to select a format
-        String format = nameParse.nextLine(); // holds user input
+        String format = nameParse.nextLine(); // holds user input to format of name
 
-        if (format.equalsIgnoreCase("with middle")){
+        if (format.equalsIgnoreCase("with middle")){ // if with middle was chosen run this
             System.out.println("You chose a format with a middle name");
-            System.out.println("Enter your name");
-            String fullName = nameParse.nextLine().trim();
+            System.out.println("Enter your name"); // ask user to input their name
+            String fullName = nameParse.nextLine().trim(); // holds user name
 
-            String[] withMiddle = fullName.split(Pattern.quote(" "));
+            String[] withMiddle = fullName.split(Pattern.quote(" ")); // turn their name into an array. item in array is separated by a 'space'
             System.out.println("First Name: " + withMiddle[0]);
-            System.out.println("Middle Name: " + withMiddle[1]);
+            System.out.println("Middle Name: " + withMiddle[1]);  // output of their name assigning each array item to a line
             System.out.println("Last Name: " + withMiddle[2]);
         }
-        else {
+        else {                                                      // if without middle was chosen run this
             System.out.println("You chose a format without a middle name");
-            System.out.println(" Enter your name");
-            String fullName = nameParse.nextLine().trim();
+            System.out.println(" Enter your name"); // ask user for their name
+            String fullName = nameParse.nextLine().trim(); // holds user name
 
-            String[] woMiddle = fullName.split(Pattern.quote(" "));
+            String[] woMiddle = fullName.split(Pattern.quote(" ")); // turn that name into an array
             System.out.println("First Name: " + woMiddle[0]);
-            System.out.println("Last Name: " + woMiddle[1]);
+            System.out.println("Last Name: " + woMiddle[1]);  // output their name, assigning an item per line
 
         }
     }
